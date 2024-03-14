c66d3121619dd6b7c0a78b74f8205d6ba7b98a85
staroperator <staroperator92@gmail.com>
2024-03-13 23:35:34 -0400

    semantics for LK

--- SequentCalculus.lean
+++ SequentCalculus.lean
@@ -3,3 +3,4 @@ import SequentCalculus.Propositional.Syntax
 import SequentCalculus.Propositional.LJ.Sequent
 import SequentCalculus.Propositional.LJ.Semantics
 import SequentCalculus.Propositional.LK.Sequent
+import SequentCalculus.Propositional.LK.Semantics
